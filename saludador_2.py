929578c09c5d496b5852be0001bbd69f19a23d09
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2019-09-26 09:41:19 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,9 @@
+def saludador_2():
+    print "Hola amigo mio" #Instruccion de salida
+    nombre=raw_input("Como te llamas?:") #Instruccion de entrada
+    print "Bienvenido a Python"+ nombre
+    edad=raw_input("Cual es tu edad?:") #Instruccion de entrada
+    print "Me gusta tu edad"
+    
+saludador_2()
+ 
