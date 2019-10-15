32ac2be44b0cc5c0f829948960b125f496c0e9b1
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2019-10-15 11:56:46 +0200

    Add files via upload

---
+++ pares_impares.py
@@ -0,0 +1,12 @@
+def pares_impares():
+    numero=input("Dime un numero:")
+    if numero%2==0:
+        print "Este numero es par"
+    else:
+        print "Este numero es impar"
+    if numero%3==0:
+        print "Y es multiplo de tres"
+    else:
+        print "Pero no es multiplo de tres"
+
+pares_impares()
